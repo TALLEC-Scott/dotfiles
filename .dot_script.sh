c208d71b1f5a5616a26195e0f027a938253b825b
Scott Tallec <63871153+TALLEC-Scott@users.noreply.github.com>
2025-07-13 19:17:40 +0200

    update install script to use uv instead of pipx

--- .dot_script.sh
+++ .dot_script.sh
@@ -1,3 +1,23 @@
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env #refresh
+
+# list of tools, should perhaps create a seperate file for this
+tools=(
+  ruff@latest
+  nvitop
+  speedtest-cli
+  cowsay #integral to my setup
+  
+)
+
+# Install each one in turn
+for t in "${tools[@]}"; do
+  uv tool install "$t"
+done
+
+uv python install 3.12 --default --preview
+
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sudo apt install build-essential cmake vim-nox python3-dev
